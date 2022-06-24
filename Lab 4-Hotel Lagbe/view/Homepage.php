d8484b0aa370ee4e86f32d3e109baeca419eae1d
SabitTheSyed <106464872+SabitTheSyed@users.noreply.github.com>
2022-06-24 11:40:22 -0700

    Lab-4

---
+++ Lab 4-Hotel Lagbe/view/Homepage.php
@@ -0,0 +1,45 @@
+<?php 
+	include('include/Header.php') 
+?>
+
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Home
+	</title>
+</head>  
+<body>
+	
+	<center>
+		<br><br>
+
+		<img src="../model/4.jpg" width="470" height="150">
+
+		<br><br>
+
+
+		<h1> Wellcome</h1>
+
+		<h3>Short Description:</h3>
+		
+
+		<p>These projects designed to make us think about what are we learning in a slightly different way and to gain experience. <br>We choose hotel booking system to prevent all the manual problems and make our life easier. <br>The main aim of our project is to provide a paper-less hotel booking system .It also aims at hassle less and reliable automation of existing system.</p>
+
+		<br><br>
+		<img src="../model/1.jpg" width="500" height="160">
+		<br><br>
+		
+	
+
+	
+	
+
+	
+
+	
+</center>
+</body>
+</html>
+<?php include('include/Footer.php');?>
